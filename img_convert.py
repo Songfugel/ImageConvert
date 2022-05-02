a020d356c8c606a4cf61751959fd6051e3c9fc42
Santtu <kahkonen@hotmail.com>
2022-05-02 20:27:38 +0300

    Updated help text to match default settings

--- img_convert.py
+++ img_convert.py
@@ -18,14 +18,14 @@ def main():
 
     Can either use console parameters or a json settings file to provide default values / extra functionality that is desired
 
-    -t | --thumb                Generates thumbnail file (default 300x300) with suffix _thumb
-    -s | --small                Generates Small file (default 300x300) with suffix _s
+    -t | --thumb                Generates thumbnail file (default 150x150) with suffix _thumb
+    -s | --small                Generates Small file (default 150x150) with suffix _s
     -m | --medium               Generates Medium file (default 300x300) with suffix _m
     -l | --large                Generates Large file (default 1024x1024) with suffix _l
-    st:'%size%'                Define custom square size for thumbnail file
-    ss:'%size%'                Define custom square size for small file
-    sm:'%size%'                Define custom square size for medium file
-    sl:'%size%'                Define custom square size for large file
+    st:'%size%'                 Define custom square size for thumbnail file
+    ss:'%size%'                 Define custom square size for small file
+    sm:'%size%'                 Define custom square size for medium file
+    sl:'%size%'                 Define custom square size for large file
     from:'%file_type%'          Converts files from the requested type e.g.: from:'.jpg'
     to:'%file_type%'            Converts files to the requested type e.g.: to:'.png'
     f:'%directory_path%'        Allow you to define the path to the directory where you want to convert
